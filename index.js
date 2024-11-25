2233692e7d56f894250ab72558f8e873082acebd
Your Name <you@example.com>
2024-11-25 15:32:48 -0500

    eeeee

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(inputEl.value)
     var listed = "";
+    ulEl.innerHTML = ""
     for (var i = 0; i < myLeads.length; i++) {
         listed += myLeads[i] + " "
+        const li = document.createElement("li") 
+        const link = document.createElement("a")
+        link.textContent = myLeads[i]
+        link.href = "'" + myLeads[i] + "'"
+        li.innerHTML = link.innerHTML
+        ulEl.append(li)
+
     }
     console.log(listed)
+   
+     
  })
+
+
 
 
  
