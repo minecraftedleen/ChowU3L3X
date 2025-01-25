2942f10d4e04e4ad981ac4017a8bf84a13a4602b
edmund <sample@bths.edu>
2025-01-24 22:18:36 -0500

    parta

--- index.js
+++ index.js
@@ -8,20 +8,13 @@ console.log(ulEl)
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(inputEl.value)
-    var listed = "";
-    ulEl.innerHTML = ""
+    var listItems = ""
     for (var i = 0; i < myLeads.length; i++) {
-        listed += myLeads[i] + " "
-        var li = document.createElement("li") 
-        var link = document.createElement("a")
-        link.textContent= myLeads[i]
-        link.href=myLeads[i]
-        li.innerHTML = link.outerHTML
-        ulEl.append(li)
+        listItems += "<li>" + myLeads[i]
     }
-    console.log(listed)
-   
-     
+    console.log(myLeads)
+    ulEl.innerHTML=listItems
+    console.log(ulEl.innerHTML)
  })
 
 
